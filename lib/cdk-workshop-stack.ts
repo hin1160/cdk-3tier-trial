4107e035d7a4977a34099a5861ebc3d4c3a1efca
Workshop Participant <participant@workshops.aws>
2025-02-04 06:37:51 +0000

    2台目のWebServerを追加

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -4,8 +4,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as rds from 'aws-cdk-lib/aws-rds';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2'; 
 import * as targets from 'aws-cdk-lib/aws-elasticloadbalancingv2-targets';
-import { readFileSync } from 'fs'; // ファイルを読み込むためのパッケージ
-import { CfnOutput } from 'aws-cdk-lib'; //CFnOutput
+import { WebServerInstance } from './constructs/web-server-instance'; // 自作コンストラクトを import
 
 //ここからリソースを作成
 export class CdkWorkshopStack extends Stack {
@@ -17,32 +16,16 @@ export class CdkWorkshopStack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
     });
 
-    //ec2を宣言
-    const webServer1 = new ec2.Instance(this, "WordPressServer1", {
-      //インスタンスを配置するVPCを指定
-      vpc,
-      //インスタンスタイプを指定
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
-      //マシンイメージを指定
-      machineImage: new ec2.AmazonLinuxImage({
-        generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
-      }),
-      //インスタンスを配置するサブネットを指定
-      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC},
-
+    //新しく作成したコンストラクタを使用してインスタンスを宣言
+    //1台目
+    const webServer1 = new WebServerInstance(this, "WebServer1", {
+      vpc
     });
+    //2台目
+    const webServer2 = new WebServerInstance(this, "WebServer2", {
+      vpc
+    });    
 
-    //user-date.shを読み込み、変数に格納
-    const script =  readFileSync("./lib/resources/user-data.sh", "utf8");
-
-    //EC2インスタンスにユーザーデータを追加
-    webServer1.addUserData(script);
-
-    //インスタンスのパブリックIPを含むURLを出力
-    new CfnOutput(this, "WordpressServer1PublicIPAddress", {
-      value: `http://${webServer1.instancePublicIp}`
-    });
-    
 
     //rdsを宣言
     const rdsServer1 = new rds.DatabaseInstance(this, "primaryRdsServer1", {
@@ -52,31 +35,34 @@ export class CdkWorkshopStack extends Stack {
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.SMALL),
     });
 
-    //webServerからのアクセスのみを許可
-    rdsServer1.connections.allowDefaultPortFrom(webServer1);
-  
-  //ALBを宣言
-  const alb = new elbv2.ApplicationLoadBalancer(this, "wordPressALB", {
-    vpc,
-    internetFacing: true
-  });
+    //アクセス許可対象をコンストラクト内のインスタンスに変更
+    //1台目のWebServerへのアクセス許可
+    rdsServer1.connections.allowDefaultPortFrom(webServer1.instance);
+    //2台目のWebServerへのアクセス許可
+    rdsServer1.connections.allowDefaultPortFrom(webServer2.instance);
+    
+    //ALBを宣言
+    const alb = new elbv2.ApplicationLoadBalancer(this, "wordPressALB", {
+      vpc,
+      internetFacing: true
+    });
 
-  //リスナーを追加
-  const albListner = alb.addListener("wordPressALBListner", {port: 80});
+    //リスナーを追加
+    const albListner = alb.addListener("wordPressALBListner", {port: 80});
 
-  //ターゲットとなるインスタンスを宣言
-  const instanceTarget = new targets.InstanceTarget(webServer1, 80); 
-  
-  //ターゲットをリスナーに追加
-  //addListnerメソッドがApplicationListnerを返すので、ApplicationListnerクラスのメソッドが使えるようになる
-  albListner.addTargets("ALBListnerTargets", {
-    port: 80,
-    targets: [instanceTarget],
-    healthCheck: {path : "/wp-includes/images/blank.gif"}, //HealthCheck型のオブジェクト
-  })  
+    //ターゲットとなるインスタンスを宣言
+    const instanceTarget = new targets.InstanceTarget(webServer1.instance, 80); 
+    
+    //ターゲットをリスナーに追加
+    //addListnerメソッドがApplicationListnerを返すので、ApplicationListnerクラスのメソッドが使えるようになる
+    albListner.addTargets("ALBListnerTargets", {
+      port: 80,
+      targets: [instanceTarget],
+      healthCheck: {path : "/wp-includes/images/blank.gif"}, //HealthCheck型のオブジェクト
+    })  
 
-  //ALBからEC2へのアクセスを許可
-  webServer1.connections.allowFrom(alb,ec2.Port.tcp(80));
+    //ALBからEC2へのアクセスを許可
+    webServer1.instance.connections.allowFrom(alb,ec2.Port.tcp(80));
 
   }
 }
